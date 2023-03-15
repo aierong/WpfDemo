4601963f2ae98bc5a35692aaadf2f04106b52bee
aierong <aierong@126.com>
2023-03-15 14:02:34 +0800

    0

--- WpfApp/Models/Student.cs
+++ WpfApp/Models/Student.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace WpfApp.Models
 {
-    public  class Student
+    public class Student
     {
+        public string Age
+        {
+            get; set;
+        }
 
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Birthday
+        {
+            get; set;
+        }
+
+        public string Country
+        {
+            get; set;
+        }
     }
 }
