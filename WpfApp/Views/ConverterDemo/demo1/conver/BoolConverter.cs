9160f6421264b9db5a6718fb459b3f0938f35b57
aierong <aierong@126.com>
2023-03-22 13:05:36 +0800

    1

---
+++ WpfApp/Views/ConverterDemo/demo1/conver/BoolConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+
+
+namespace WpfApp.Views.ConverterDemo.demo1.conver
+{
+    public class BoolConverter : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert ( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+            //viewmodel中的值，转换为view中的值 
+
+            object obj = value;
+            if ( obj is bool )
+            {
+                bool path = ( bool ) obj;
+
+                return path ? "真" : "假";
+            }
+
+            return "假";
+        }
+
+
+
+        public object ConvertBack ( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+            //当界面的值发生变化时，会调用该方法 view中的值转换为viewmodel
+
+
+            if ( value != null )
+            {
+                if ( !string.IsNullOrEmpty( value.ToString() ) )
+                {
+                    if ( value.ToString() == "真" )
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
---
+++ WpfApp/Views/ConverterDemo/demo1/viewmodel/WindowViewModel1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+namespace WpfApp.Views.ConverterDemo.demo1.viewmodel
+{
+    public class WindowViewModel1 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public WindowViewModel1 ()
+        {
+            Name = "Name_init";
+            IsFlag = true;
+
+            ShowCommand = new MyCommand( Show );
+        }
+
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public MyCommand ShowCommand
+        {
+            get; set;
+        }
+
+
+
+        public void Show ()
+        {
+            Name = "改了";
+            IsFlag = !IsFlag;
+
+            MessageBox.Show( "show Command" );
+        }
+
+
+        private string name;
+
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Name" ) );
+            }
+        }
+
+
+
+        private bool isflag;
+
+
+        /// <summary>
+        ///  
+        /// </summary>
+        public bool IsFlag
+        {
+            get => isflag;
+
+            set
+            {
+                isflag = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "IsFlag" ) );
+            }
+        }
+
+
+
+    }
+}
