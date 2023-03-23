49cdf79589f48813d8c4e72bf42296a6640e0b68
aierong <aierong@126.com>
2023-03-23 15:56:35 +0800

    1

--- CommunityToolkitMvvmDemo/Views/Window1.xaml.cs
+++ CommunityToolkitMvvmDemo/Views/Window1.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CommunityToolkitMvvmDemo
+namespace CommunityToolkitMvvmDemo.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Window1.xaml 的互動邏輯
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow ()
+        public Window1 ()
         {
             InitializeComponent();
         }
