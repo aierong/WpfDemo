d3334dcf3e505f26a9f1815d861d034a793862f0
aierong <aierong@126.com>
2023-03-23 16:03:54 +0800

    1

--- CommunityToolkitMvvmDemo/ViewModels/UC/StudentForm.cs
+++ CommunityToolkitMvvmDemo/ViewModels/UC/StudentForm.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommunityToolkitMvvmDemo.ViewModels
+namespace CommunityToolkitMvvmDemo.ViewModels.UC
 {
-    internal class zw
+    internal class StudentForm
     {
     }
 }
