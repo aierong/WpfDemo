985e0affca42d41500016701207a9c0b2b393af0
aierong <aierong@126.com>
2023-03-23 15:53:00 +0800

    1

---
+++ CommunityToolkitMvvmDemo/Views/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityToolkitMvvmDemo.Views
+{
+    class Class1
+    {
+    }
+}
