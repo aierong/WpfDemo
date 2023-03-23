94f2f3037a6882c35864764e6bcb3da33599821b
aierong <aierong@126.com>
2023-03-23 16:58:04 +0800

    0

---
+++ CommunityToolkitMvvmDemo/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityToolkitMvvmDemo.Models
+{
+    public class Student
+    {
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Class
+        {
+            get; set;
+        }
+
+        public string Phone
+        {
+            get; set;
+        }
+    }
+}
--- CommunityToolkitMvvmDemo/ViewModels/UC/StudentListViewModel.cs
+++ CommunityToolkitMvvmDemo/ViewModels/UC/StudentListViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkitMvvmDemo.Models;
 
 namespace CommunityToolkitMvvmDemo.ViewModels.UC
 {
     public partial class StudentListViewModel : ObservableObject
     {
+        public ObservableCollection<Student> Students
+        {
+            get;
+        } = new ObservableCollection<Student>() { };
+
+
+
     }
 }
