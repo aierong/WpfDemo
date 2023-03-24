7db6cdf5df35cc64b1c4b8e14d0cd7a1d2cf571a
aierong <aierong@126.com>
2023-03-24 20:14:31 +0800

    1

--- WpfDemoNet6/IOCDemo/ViewModels/WindowViewModel1.cs
+++ WpfDemoNet6/IOCDemo/ViewModels/WindowViewModel1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace WpfDemoNet6.IOCDemo.ViewModels
 {
-    public  class WindowViewModel1
+    public class WindowViewModel1 : ObservableObject
     {
     }
 }
--- WpfDemoNet6/IOCDemo/Views/Window1.xaml.cs
+++ WpfDemoNet6/IOCDemo/Views/Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfDemoNet6.IOCDemo.ViewModels;
 
 namespace WpfDemoNet6.IOCDemo.Views
 {
@@ -22,6 +23,7 @@ namespace WpfDemoNet6.IOCDemo.Views
         public Window1 ()
         {
             InitializeComponent();
+            this.DataContext = new WindowViewModel1();
         }
     }
 }
