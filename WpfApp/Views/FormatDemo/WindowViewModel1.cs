0e3c09466e44e06e8b379deb9d1c6ccf87416ef8
aierong <aierong@126.com>
2023-03-25 11:18:26 +0800

    1

---
+++ WpfApp/Views/FormatDemo/WindowViewModel1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Views.FormatDemo
+{
+    public class WindowViewModel1 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private decimal  price;
+
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Price" ) );
+            }
+        }
+
+        private string name;
+
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Name" ) );
+            }
+        }
+
+        private string lastname;
+
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "LastName" ) );
+            }
+        }
+    }
+}
