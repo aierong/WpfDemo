1099b8d0868bac202d79ac20bef65c1532eae2d5
aierong <aierong@126.com>
2023-03-28 19:04:07 +0800

    1

---
+++ WpfApp/Views/BindData/ListData/plus/Models/EMP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Views.BindData.ListData.plus.Models
+{
+    public class EMP : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Id
+        {
+            get; set;
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Name" ) );
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Age" ) );
+            }
+        }
+    }
+}
---
+++ WpfApp/Views/BindData/ListData/plus/ViewModels/PlusDataViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Views.BindData.ListData.plus.Models;
+
+namespace WpfApp.Views.BindData.ListData.plus.ViewModels
+{
+    public class PlusDataViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public BindingList<EMP> Students
+        {
+            get; set;
+        } = new BindingList<EMP>();
+
+        public PlusDataViewModel ()
+        {
+        }
+    }
+}
