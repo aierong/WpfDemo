ee16582dfbf8c230333c392db46d5204bfbcddd6
aierong <aierong@126.com>
2023-03-28 16:27:43 +0800

    怕

--- WpfApp/Views/BindData/ListData/ViewModels/ListDataViewModel.cs
+++ WpfApp/Views/BindData/ListData/ViewModels/ListDataViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfApp.Views.BindData.ListData.Models;
 
 namespace WpfApp.Views.BindData.ListData.ViewModels
@@ -12,13 +13,12 @@ namespace WpfApp.Views.BindData.ListData.ViewModels
     public class ListDataViewModel : INotifyPropertyChanged
     {
 
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ObservableCollection<Student> Students
         {
-            get;
-        } 
+            get; set;
+        }
 
         public ListDataViewModel ()
         {
@@ -30,8 +30,43 @@ namespace WpfApp.Views.BindData.ListData.ViewModels
                 new Student(){ Id=4, Age=14, Name="Andy"}
             };
 
-            Name = "Init";
+
+            //在添加、删除或移动项或刷新整个列表时发生。
+            Students.CollectionChanged += ( object sender , System.Collections.Specialized.NotifyCollectionChangedEventArgs e ) =>
+            {
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Total" ) );
+            };
+
+            UpdateCommand = new MyCommands( () =>
+           {
+               if ( Students.Count >= 2 )
+               {
+                   Students.RemoveAt( 0 );
+                   Students.RemoveAt( 0 );
+               }
+
+               Students.Add( new Student() { Id = Students.Count >= 1 ? Students.Max( item => item.Id + 1 ) : 1 , Age = 18 , Name = "guoguo" } );
+
+
+               //通知数据已经变化  这里可以不写了,因为注册了事件CollectionChanged
+               //PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Total" ) );
+           } );
+
+            AllUpdateCommand = new MyCommands( () =>
+            {
+                Students = new ObservableCollection<Student>() {
+                new Student(){ Id=111, Age=11, Name="Tom"},
+                new Student(){ Id=222, Age=12, Name="Darren"},
+                new Student(){ Id=333, Age=13, Name="Jacky"},
+                new Student(){ Id=444, Age=14, Name="Andy"}
+                 };
+
+
+                //通知数据已经变化 这里可以不写了,因为注册了事件CollectionChanged
+                //PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Total" ) );
+            } );
         }
+
 
 
         private string name;
@@ -54,5 +89,42 @@ namespace WpfApp.Views.BindData.ListData.ViewModels
             }
         }
 
+
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public MyCommands UpdateCommand
+        {
+            get; set;
+        }
+
+
+        public MyCommands AllUpdateCommand
+        {
+            get; set;
+        }
+
+
+
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public int Total
+        {
+            get => Students != null & Students.Count > 0 ? Students.Sum( item => item.Age ) : 0;
+
+        }
+
+
+
+
+
+
+        //private void Students_CollectionChanged ( object sender , System.Collections.Specialized.NotifyCollectionChangedEventArgs e )
+        //{
+        //    PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Total" ) );
+        //}
     }
 }
---
+++ WpfApp/Views/BindData/ListData/ViewModels/MyCommands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp.Views.BindData.ListData.ViewModels
+{
+    public class MyCommands : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        Action execAction;
+
+
+        public MyCommands ( Action action )
+        {
+            execAction = action;
+        }
+
+        public bool CanExecute ( object parameter )
+        {
+            return true;
+        }
+
+        public void Execute ( object parameter )
+        {
+            execAction();
+        }
+    }
+}
