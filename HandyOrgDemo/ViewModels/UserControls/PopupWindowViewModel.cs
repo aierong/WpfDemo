23874a024acb3e6d461e03eead5f9f8bb1a17a0b
aierong <aierong@126.com>
2023-04-02 15:41:35 +0800

    /

---
+++ HandyOrgDemo/ViewModels/UserControls/PopupWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HandyOrgDemo.Views.UserControls;
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public partial class PopupWindowViewModel : ObservableObject
+    {
+        [RelayCommand()]
+        void ButtonClick ()
+        {
+            //点击按钮 
+
+            //Debug.WriteLine( "ButtonClick" );
+
+            PopupWindowDemo1 popwin = new PopupWindowDemo1();
+            HandyControl.Controls.PopupWindow pwin = new HandyControl.Controls.PopupWindow();
+            pwin.PopupElement = popwin;
+
+            //调用另一个用户控件,弹窗
+            pwin.Show();
+            //模态化显示窗口
+            //pwin.ShowDialog ();
+        }
+
+        public PopupWindowViewModel ()
+        {
+
+        }
+    }
+}
