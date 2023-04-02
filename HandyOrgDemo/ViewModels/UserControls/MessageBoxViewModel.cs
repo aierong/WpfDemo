c0e68871ff29e12b0c2fead621fa8687a26d78ef
aierong <aierong@126.com>
2023-04-02 15:14:45 +0800

    1

---
+++ HandyOrgDemo/ViewModels/UserControls/MessageBoxViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public partial class MessageBoxViewModel : ObservableObject
+    {
+        [RelayCommand()]
+        void ButtonClick ()
+        {
+            //点击按钮 
+
+            //Debug.WriteLine( "ButtonClick" );
+
+            HandyControl.Controls.MessageBox.Success( "运行成功" , "OK了" );
+            HandyControl.Controls.MessageBox.Error( "错误了" , "error了" );
+        }
+
+        [RelayCommand()]
+        void ButtonAskClick ()
+        {
+            //点击按钮 
+
+            //Debug.WriteLine( "ButtonClick" );
+
+            var dlg = HandyControl.Controls.MessageBox.Show( "运行吗?" , "确定" , MessageBoxButton.YesNo );
+
+            if ( dlg == MessageBoxResult.Yes )
+            {
+                HandyControl.Controls.MessageBox.Success( "你点击了yes" );
+            }
+        }
+
+        public MessageBoxViewModel ()
+        {
+
+        }
+    }
+}
