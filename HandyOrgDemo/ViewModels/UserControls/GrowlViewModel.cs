0ff384b49d9e653c7381ccdf34e0b81efcf577fc
aierong <aierong@126.com>
2023-04-03 15:46:05 +0800

    111

--- HandyOrgDemo/ViewModels/UserControls/GrowlViewModel.cs
+++ HandyOrgDemo/ViewModels/UserControls/GrowlViewModel.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace HandyOrgDemo.ViewModels.UserControls
 {
     public partial class GrowlViewModel : ObservableObject
     {
+        [RelayCommand()]
+        void ButtonClick ()
+        {
+            //点击按钮 
+
+            //Debug.WriteLine( "ButtonClick" );
+
+            //HandyControl.Controls.Growl.Success( "运行成功" );   //提示会在窗体内部现实
+            HandyControl.Controls.Growl.SuccessGlobal( "运行成功1" );  //提示会在窗体外部现实
+
+            //HandyControl.Controls.Growl.Success( "运行成功1" , GrowlScriptToken );
+        }
+
+        public static readonly string GrowlScriptToken = "msgone";
+
+        public GrowlViewModel ()
+        {
+
+        }
     }
 }
--- HandyOrgDemo/Views/UserControls/GrowlDemoUserControl.xaml.cs
+++ HandyOrgDemo/Views/UserControls/GrowlDemoUserControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HandyOrgDemo.ViewModels.UserControls;
 
 namespace HandyOrgDemo.Views.UserControls
 {
@@ -23,6 +24,8 @@ namespace HandyOrgDemo.Views.UserControls
         public GrowlDemoUserControl ()
         {
             InitializeComponent();
+
+            this.DataContext = new GrowlViewModel();
         }
     }
 }
