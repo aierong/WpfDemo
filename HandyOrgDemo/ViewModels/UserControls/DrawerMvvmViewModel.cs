98582c3cb74b71c92d0f17eeea9500a70a3352b0
aierong <aierong@126.com>
2023-04-05 11:53:40 +0800

    1

---
+++ HandyOrgDemo/ViewModels/UserControls/DrawerMvvmViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HandyControl.Data;
+using HandyOrgDemo.Models;
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public partial class DrawerMvvmViewModel : ObservableObject
+    {
+        [RelayCommand()]
+        void LeftButtonClick ()
+        {
+            //点击按钮 
+
+            IsLeftOpen = true;
+        }
+
+
+
+        [RelayCommand()]
+        void TopButtonClick ()
+        {
+            //点击按钮 
+
+            IsTopOpen = true;
+        }
+
+        [ObservableProperty]
+        private bool isLeftOpen = false;
+
+        [ObservableProperty]
+        private bool isTopOpen = false;
+
+
+        [RelayCommand()]
+        void LeftCloseButtonClick ()
+        {
+            //点击按钮 
+
+            IsLeftOpen = false;
+        }
+
+        public DrawerMvvmViewModel ()
+        {
+
+        }
+    }
+}
