220f9b5991c1869b43f29fb80cc35470af6039ca
aierong <aierong@126.com>
2023-04-11 19:56:29 +0800

    1

---
+++ PrismDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace PrismDemo.ViewModels
+{
+    public class MainWindowViewModel: BindableBase
+    {
+        private string _text="init";
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                SetProperty( ref _text , value );
+            }
+        }
+
+        private string _texts = "inits";
+
+        public string TextS
+        {
+            get
+            {
+                return _texts;
+            }
+            set
+            {
+                SetProperty( ref _texts , value );
+            }
+        }
+    }
+}
