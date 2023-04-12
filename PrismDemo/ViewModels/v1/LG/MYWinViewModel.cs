0e5501470a43f5b76b78d872e1cefb546d7729e6
aierong <aierong@126.com>
2023-04-12 09:35:51 +0800

    0

---
+++ PrismDemo/ViewModels/v1/LG/MYWinViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace PrismDemo.ViewModels.v1.LG
+{
+    public class MYWinViewModel : BindableBase
+    {
+        private string _texts = "init_abc";
+
+        public string TextS
+        {
+            get
+            {
+                return _texts;
+            }
+            set
+            {
+                SetProperty( ref _texts , value );
+            }
+        }
+    }
+}
---
+++ PrismDemo/Views/v1/LG/MYWin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrismDemo.Views.v1.LG
+{
+    /// <summary>
+    /// MYWin.xaml 的互動邏輯
+    /// </summary>
+    public partial class MYWin : Window
+    {
+        public MYWin ()
+        {
+            InitializeComponent();
+        }
+    }
+}
