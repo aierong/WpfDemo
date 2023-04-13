e6da695b97854dd33a0bc5846612da92886cde51
aierong <aierong@126.com>
2023-04-13 14:27:36 +0800

    0

---
+++ PrismDemo/EventDemo/Model/MyMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismDemo.EventDemo.Model
+{
+    public class MyMessage
+    {
+        public string Datas;
+
+        public int Ids;
+    }
+}
