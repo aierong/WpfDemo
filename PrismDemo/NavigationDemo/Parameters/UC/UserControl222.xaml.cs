5a16399d25360a40bd084f8299e5fc2ba8e2aa29
aierong <aierong@126.com>
2023-04-14 13:21:05 +0800

    1

---
+++ PrismDemo/NavigationDemo/Parameters/UC/UserControl222.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrismDemo.NavigationDemo.Parameters.UC
+{
+    /// <summary>
+    /// UserControl222.xaml 的互動邏輯
+    /// </summary>
+    public partial class UserControl222 : UserControl
+    {
+        public UserControl222 ()
+        {
+            InitializeComponent();
+        }
+    }
+}
