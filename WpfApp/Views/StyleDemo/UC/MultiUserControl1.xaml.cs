c2d117f7d7864421380ff556493d0c07816a959a
aierong <aierong@126.com>
2023-05-05 16:20:35 +0800

    0

---
+++ WpfApp/Views/StyleDemo/UC/MultiUserControl1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.StyleDemo.UC
+{
+    /// <summary>
+    /// MultiUserControl1.xaml 的互動邏輯
+    /// </summary>
+    public partial class MultiUserControl1 : UserControl
+    {
+        public MultiUserControl1 ()
+        {
+            InitializeComponent();
+        }
+    }
+}
