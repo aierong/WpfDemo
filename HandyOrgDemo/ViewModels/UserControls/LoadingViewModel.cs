6a013a95a5d9845bc647259071b61ac09252561e
aierong <aierong@126.com>
2023-05-07 11:09:03 +0800

    1

---
+++ HandyOrgDemo/ViewModels/UserControls/LoadingViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public partial class LoadingViewModel : ObservableObject
+    {
+        [RelayCommand()]
+        void ButtonClick ()
+        {
+            //点击按钮 
+
+            //Debug.WriteLine( "ButtonClick" );
+
+
+        }
+
+
+    }
+}
---
+++ HandyOrgDemo/Views/UserControls/LoadingUserControl1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HandyOrgDemo.ViewModels.UserControls;
+
+namespace HandyOrgDemo.Views.UserControls
+{
+    /// <summary>
+    /// LoadingUserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoadingUserControl1 : UserControl
+    {
+        public LoadingUserControl1 ()
+        {
+            InitializeComponent();
+
+            this.DataContext = new LoadingViewModel();
+        }
+    }
+}
