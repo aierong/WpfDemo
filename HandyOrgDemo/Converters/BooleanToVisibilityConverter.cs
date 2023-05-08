cf249bda0f49e0eb3fd373bc29da6614875f6b0f
aierong <aierong@126.com>
2023-05-08 11:18:46 +0800

    1

---
+++ HandyOrgDemo/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HandyOrgDemo.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// 假时用什么模式
+        /// </summary>
+        public bool FalseUseHidden
+        {
+            get; set;
+        }
+
+
+
+        public object Convert ( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+            return ( bool ) value ? Visibility.Visible : ( FalseUseHidden ? Visibility.Hidden : Visibility.Collapsed );
+        }
+
+        public object ConvertBack ( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+            if ( value is Visibility )
+            {
+                Visibility v = ( Visibility ) value;
+
+                if ( v == Visibility.Visible )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
