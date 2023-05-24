478ef8c480d096d92b037ea620bfa2c809e14740
aierong <aierong@126.com>
2023-05-24 08:45:19 +0800

    1

--- ChartsDemo/App.xaml.cs
+++ ChartsDemo/App.xaml.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
 
 namespace ChartsDemo
 {
     /// <summary>
     /// App.xaml 的互動邏輯
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        /// <summary>
+        /// 设置启动起始页
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell ()
+        {
+
+            return Container.Resolve<Views.Window1>();
+        }
+
+
+        protected override void RegisterTypes ( IContainerRegistry containerRegistry )
+        {
+            //注册导航
+            //注册对话框
+
+        }
+
+
     }
 }
--- ChartsDemo/Views/MainPage.xaml.cs
+++ ChartsDemo/Views/MainPage.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChartsDemo
+namespace ChartsDemo.Views
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// MainPage.xaml 的互動邏輯
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Window
     {
-        public MainWindow ()
+        public MainPage ()
         {
             InitializeComponent();
         }
