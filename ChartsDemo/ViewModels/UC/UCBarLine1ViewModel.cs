7c00ba4b4fda71c8edb60d6a8dd9a95ab77ab0e3
aierong <aierong@126.com>
2023-05-24 16:49:11 +0800

    0

--- ChartsDemo/ViewModels/UC/UCBarLine1ViewModel.cs
+++ ChartsDemo/ViewModels/UC/UCBarLine1ViewModel.cs
@@ -13,51 +13,61 @@ namespace ChartsDemo.ViewModels.UC
 {
     public class UCBarLine1ViewModel : BindableBase
     {
+
         public ISeries[] Series
         {
             get; set;
-        } =     {
-            //多组数据
+        } =
+           {
+                new ColumnSeries<double> { Values = new double[] { 426, 583, 104 } },
+                new LineSeries<double>   { Values = new double[] { 200, 558, 458 }, Fill = null },
+            
+            };
 
-                new ColumnSeries<double>
-                {
-                    //Name = "Mary姐姐",
-                    Values = new double[] { 2, 5, 4 }
-                },
-
-
-                new LineSeries<double>   { Values = new double[] { 200, 558, 458 }, Fill = null }
-
-
-        };
 
 
         public Axis[] XAxes
         {
             get; set;
         } =
-           {
-                new Axis
+        {
+            new Axis
+            {
+                Name = "Salesman/woman",
+                Labels = new string[] { "王", "赵", "张" },
+                LabelsPaint = new SolidColorPaint
                 {
-                    //标题
-                    Name = "Salesman/woman",
-                    //  labels 
-                    Labels = new string[] { "Sergio", "Lando弟弟", "Lewis姐姐" },
-                    //旋转角度
-                    LabelsRotation = 1,
-                    LabelsPaint = new SolidColorPaint
-                    {
-                        Color = SKColors.Black,
+                    Color = SKColors.Black,
 
-                        SKTypeface = SKFontManager.Default.MatchCharacter('汉') // 汉语 
-                        // SKTypeface = SKFontManager.Default.MatchCharacter('أ'), // Arab
-                        // SKTypeface = SKFontManager.Default.MatchCharacter('あ'), // Japanese
-                        // SKTypeface = SKFontManager.Default.MatchCharacter('Ж'), // Russian
-                    }
+                    SKTypeface = SKFontManager.Default.MatchCharacter('汉') // 汉语 
+                    // SKTypeface = SKFontManager.Default.MatchCharacter('أ'), // Arab
+                    // SKTypeface = SKFontManager.Default.MatchCharacter('あ'), // Japanese
+                    // SKTypeface = SKFontManager.Default.MatchCharacter('Ж'), // Russian
+                                 
                 }
-            };
+            }
+        };
 
 
+
+        public Axis[] YAxes
+        {
+            get; set;
+        } =
+        {
+            new Axis
+            {
+                Name = "Sales amount",
+                NamePadding = new LiveChartsCore.Drawing.Padding(0, 15),
+                //Labeler = Labelers.Currency,
+                //LabelsPaint = new SolidColorPaint
+                //{
+                //    Color = SKColors.Blue,
+                //    //FontFamily = "Times New Roman",
+                //    SKFontStyle = new SKFontStyle(SKFontStyleWeight.ExtraBold, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic)
+                //},
+            }
+        };
 
 
 
