f8c13be4629d5eb38d7ce1a45997b3ab8d810a73
aierong <aierong@126.com>
2023-05-31 10:59:47 +0800

    0

--- WpfApp/Views/KJ/img/Img2ViewModel.cs
+++ WpfApp/Views/KJ/img/Img2ViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
 using WpfApp.Views.BaseCommand;
 
 namespace WpfApp.Views.KJ.img
 {
-    public  class Img2ViewModel : INotifyPropertyChanged
+    public class Img2ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -34,19 +37,67 @@ namespace WpfApp.Views.KJ.img
         }
 
 
+        private System.Windows.Media.ImageSource imgdata = null;
+
+        public System.Windows.Media.ImageSource ImgData
+        {
+            get
+            {
+                return imgdata;
+            }
+            set
+            {
+                imgdata = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "ImgData" ) );
+            }
+        }
+
+
+
         public Img2ViewModel ()
         {
-             
+
             NetImgName = "\\\\10.12.0.151\\misfile\\test\\images\\BQC.jpg";
 
 
-            //UpdateImgCommand = new MyCommand( () =>
-            //{
-            //    Debug.WriteLine( "UpdateImgCommand:" );
+            UpdateImgCommand = new MyCommand( () =>
+            {
+                Debug.WriteLine( "UpdateImgCommand:" );
 
-            //    ImgName = "/Imgs/VER.png";
-            //} );
+                NetImgName = "\\\\10.12.0.151\\misfile\\test\\images\\zhonghe1.png";
+            } );
+
+            SelectImgCommand = new MyCommand( () =>
+            {
+                Debug.WriteLine( "SelectImgCommand:" );
+
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if ( openFileDialog.ShowDialog() == true )
+                {
+                    Uri fileUri = new Uri( openFileDialog.FileName );
+                    ImgData = new BitmapImage( fileUri );
+                }
+            } );
         }
 
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public MyCommand UpdateImgCommand
+        {
+            get; set;
+        }
+
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public MyCommand SelectImgCommand
+        {
+            get; set;
+        }
     }
 }
