e09271485a3a68d853f91cda1946d66c4b574c46
aierong <aierong@126.com>
2023-05-31 10:38:50 +0800

    1

---
+++ WpfApp/Views/KJ/img/Img2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.KJ.img
+{
+    /// <summary>
+    /// Img2.xaml 的互動邏輯
+    /// </summary>
+    public partial class Img2 : UserControl
+    {
+        public Img2 ()
+        {
+            InitializeComponent();
+
+            this.DataContext = new Img2ViewModel();
+        }
+    }
+}
