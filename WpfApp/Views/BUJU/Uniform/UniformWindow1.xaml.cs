eb2c340128bda0c990f0dea46d99e84870b32a6c
aierong <aierong@126.com>
2023-06-01 13:25:43 +0800

    1

---
+++ WpfApp/Views/BUJU/Uniform/UniformWindow1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.BUJU.Uniform
+{
+    /// <summary>
+    /// UniformWindow1.xaml 的互動邏輯
+    /// </summary>
+    public partial class UniformWindow1 : Window
+    {
+        public UniformWindow1 ()
+        {
+            InitializeComponent();
+        }
+    }
+}
