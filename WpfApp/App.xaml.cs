91148f06d8748f974b8cf97b67b50956ba686c45
aierong <aierong@126.com>
2023-06-03 23:26:18 +0800

    0

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 
@@ -14,9 +15,50 @@ namespace WpfApp
     {
         private void Application_Startup ( object sender , StartupEventArgs e )
         {
+            //xaml中Startup = "Application_Startup"
+
             var a = 1;
 
             var b = 2;
+
+            Debug.WriteLine( "Application_Startup" );
+        }
+
+        // Application_Startup   OnStartup
+
+        protected override void OnStartup ( StartupEventArgs e )
+        {
+            base.OnStartup( e );
+
+            var a = 1;
+
+            var b = 2;
+
+            Debug.WriteLine( "OnStartup" );
+        }
+
+
+
+        private void Application_Exit ( object sender , ExitEventArgs e )
+        {
+            //xaml中Exit="Application_Exit"
+            //在应用程序关闭之前发生
+            var a = 1;
+
+            var b = 2;
+        }
+
+
+
+        private void Application_Activated ( object sender , EventArgs e )
+        {
+            //xaml中Activated="Application_Activated"
+            //当应用程序成为前台应用程序时发生。 会多次发生
+            var a = 1;
+
+            var b = 2;
+
+            Debug.WriteLine( "Application_Activated" );
         }
     }
 }
