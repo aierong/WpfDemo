d93c2fbcb7e955860d68aa1bf8ea67c54da7376d
aierong <aierong@126.com>
2023-06-04 17:33:53 +0800

    1

---
+++ WpfApp/Views/KJ/grid/tiaojianxianshi/BooleanToVisibilityConverterNew.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp.Views.KJ.grid.tiaojianxianshi
+{
+    public class BooleanToVisibilityConverterNew : IValueConverter
+    {
+        public object Convert ( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+            if ( parameter != null )
+            {
+                var b = ( bool ) value;
+
+                if ( parameter.ToString() != "1" )
+                {
+                    b = !b;
+                }
+
+                return b ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return ( bool ) value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack ( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
