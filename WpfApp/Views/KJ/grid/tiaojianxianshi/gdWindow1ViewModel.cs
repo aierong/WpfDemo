433faa806cbf6c87ca3b5f9e243574313c972444
aierong <aierong@126.com>
2023-06-06 11:37:04 +0800

    0

--- WpfApp/Views/KJ/grid/tiaojianxianshi/gdWindow1ViewModel.cs
+++ WpfApp/Views/KJ/grid/tiaojianxianshi/gdWindow1ViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp.Views.BaseCommand;
 
 namespace WpfApp.Views.KJ.grid.tiaojianxianshi
 {
@@ -31,6 +32,42 @@ namespace WpfApp.Views.KJ.grid.tiaojianxianshi
                 new StudentDataNew(){ Id=8, Age=18, Name="WUBing",IsMan =true},
                 new StudentDataNew(){ Id=9, Age=19, Name="Yang",IsMan =true}
             };
+
+
+            AddCommand = new MyCommand( adddata );
+            ReCommand = new MyCommand( redata );
+        }
+
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public MyCommand AddCommand
+        {
+            get; set;
+        }
+
+        public void adddata ()
+        {
+
+            Students.Add( new StudentDataNew() { Id = 10 , Age = 20 , Name = "Yang20" , IsMan = true } );
+            Students.Add( new StudentDataNew() { Id = 11 , Age = 12 , Name = "Y12" , IsMan = false } );
+
+        }
+
+
+        public MyCommand ReCommand
+        {
+            get; set;
+        }
+
+
+        public void redata ()
+        {
+            Students.Clear();
+
+            Students.Add( new StudentDataNew() { Id = 100 , Age = 20 , Name = "Y100" , IsMan = true } );
+            Students.Add( new StudentDataNew() { Id = 110 , Age = 12 , Name = "Y110" , IsMan = false } );
 
         }
     }
