f6c915aa6b5a4666eca673bbf47028550669cf85
aierong <aierong@126.com>
2023-06-12 10:17:44 +0800

    1

---
+++ ChartsDemo/Views/UC/UCPieDontTaiPlus.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChartsDemo.Views.UC
+{
+    /// <summary>
+    /// UCPieDontTaiPlus.xaml 的互動邏輯
+    /// </summary>
+    public partial class UCPieDontTaiPlus : UserControl
+    {
+        public UCPieDontTaiPlus ()
+        {
+            InitializeComponent();
+        }
+    }
+}
