f9a27fc68bd0089a8ac665c3bd8f2d89f4e3b2dc
aierong <aierong@126.com>
2023-06-12 10:30:21 +0800

    1

--- ChartsDemo/ViewModels/UC/UCPieDontTaiPlusViewModel.cs
+++ ChartsDemo/ViewModels/UC/UCPieDontTaiPlusViewModel.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using SkiaSharp;
 
 namespace ChartsDemo.ViewModels.UC
 {
-    public  class UCPieDontTaiPlusViewModel : BindableBase, INavigationAware
+    public class UCPieDontTaiPlusViewModel : BindableBase, INavigationAware
     {
         ObservableCollection<ISeries> _Series = new ObservableCollection<ISeries>() { };
 
@@ -30,7 +33,110 @@ namespace ChartsDemo.ViewModels.UC
 
         public UCPieDontTaiPlusViewModel ()
         {
-            //this.createdata( new Random().Next( 3 , 4 ) );
+            this.createdata( new Random().Next( 2 , 5 ) );
+        }
+
+
+
+        void createdata ( int counts )
+        {
+            //特别说明：counts 最大是4
+
+            Random _random = new Random();
+
+            this.Series.Clear();
+
+
+
+            for ( int i = 1 ; i <= counts ; i++ )
+            {
+                if ( i == 1 )
+                {
+                    var num = _random.Next( 1 , 20 );
+
+                    this.Series.Add( new PieSeries<double>
+                    {
+
+                        Values = new List<double>() { num } ,
+                        //Name = "系列:" + num.ToString() ,
+                        Fill = new SolidColorPaint( SKColors.Red ) ,
+
+                        DataLabelsPaint = new SolidColorPaint( SKColors.Black ) ,
+                        DataLabelsSize = 15 ,
+
+                        DataLabelsFormatter = point => point.PrimaryValue.ToString()
+
+                    } );
+                }
+
+                if ( i == 2 )
+                {
+                    var num = _random.Next( 1 , 20 );
+
+                    this.Series.Add( new PieSeries<double>
+                    {
+
+                        Values = new List<double>() { num } ,
+                        //Name = "系列:" + num.ToString() ,
+                        Fill = new SolidColorPaint( SKColors.Yellow ) ,
+
+                        DataLabelsPaint = new SolidColorPaint( SKColors.Black ) ,
+                        DataLabelsSize = 15 ,
+
+                        DataLabelsFormatter = point => point.PrimaryValue.ToString()
+
+                    } );
+                }
+
+                if ( i == 3 )
+                {
+                    var num = _random.Next( 1 , 20 );
+
+                    this.Series.Add( new PieSeries<double>
+                    {
+
+                        Values = new List<double>() { num } ,
+                        //Name = "系列:" + num.ToString() ,
+                        Fill = new SolidColorPaint( SKColors.Blue ) ,
+
+                        DataLabelsPaint = new SolidColorPaint( SKColors.Black ) ,
+                        DataLabelsSize = 15 ,
+
+                        DataLabelsFormatter = point => point.PrimaryValue.ToString()
+
+                    } );
+                }
+
+                if ( i == 4 )
+                {
+                    var num = _random.Next( 1 , 20 );
+
+                    this.Series.Add( new PieSeries<double>
+                    {
+
+                        Values = new List<double>() { num } ,
+                        //Name = "系列:" + num.ToString() ,
+                        Fill = new SolidColorPaint( SKColors.Green ) ,
+
+                        DataLabelsPaint = new SolidColorPaint( SKColors.Black ) ,
+                        DataLabelsSize = 15 ,
+
+                        DataLabelsFormatter = point => point.PrimaryValue.ToString()
+
+                    } );
+                }
+            }
+
+
+
+
+
+
+
+
+            return;
+
+
         }
 
 
@@ -41,14 +147,10 @@ namespace ChartsDemo.ViewModels.UC
 
             //饼图的数量变化，name也变化了
 
-            //this.createdata( new Random().Next( 3 , 6 ) );
+            this.createdata( new Random().Next( 3 , 5 ) );
             return;
 
         } ) );
-
-
-
-
 
 
 
