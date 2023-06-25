d6ad0a9aaced9610b83fc53839ba2fd8695d0622
aierong <aierong@126.com>
2023-06-25 10:05:02 +0800

    8

---
+++ WpfApp/Views/KJ/grid/tiaojianxianshi/Selector/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Views.KJ.grid.tiaojianxianshi.Selector
+{
+    internal class Class1
+    {
+    }
+}
--- WpfApp/Views/KJ/grid/tiaojianxianshi/Selector/StudentTemplateSelector.cs
+++ WpfApp/Views/KJ/grid/tiaojianxianshi/Selector/StudentTemplateSelector.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace WpfApp.Views.KJ.grid.tiaojianxianshi
+namespace WpfApp.Views.KJ.grid.tiaojianxianshi.Selector
 {
     public class StudentTemplateSelector : DataTemplateSelector
     {
