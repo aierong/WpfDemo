c5f147fcd0c06861b34699750b5281128c5f8a7e
aierong <aierong@126.com>
2023-06-25 10:16:16 +0800

    0

---
+++ WpfApp/Views/KJ/grid/tiaojianxianshi/Selector/StudentAgeTemplateSelector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp.Views.KJ.grid.tiaojianxianshi.Models;
+
+namespace WpfApp.Views.KJ.grid.tiaojianxianshi.Selector
+{
+    public  class StudentAgeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ChengRenTemplate
+        {
+            get; set;
+        }
+
+        public DataTemplate NotChengRenTemplate
+        {
+            get; set;
+        }
+
+
+        public override DataTemplate SelectTemplate ( object item , DependencyObject container )
+        {
+            var model = item as StudentDataNew;
+
+            if ( model != null )
+            {
+                return model.IsChengRen ? ChengRenTemplate : NotChengRenTemplate;
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ WpfApp/Views/KJ/grid/tiaojianxianshi/dongtuplusgridViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Views.BaseCommand;
+using WpfApp.Views.KJ.grid.tiaojianxianshi.Models;
+
+namespace WpfApp.Views.KJ.grid.tiaojianxianshi
+{
+    public class dongtuplusgridViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<StudentDataNew> Students
+        {
+            get; set;
+        } = new ObservableCollection<StudentDataNew>();
+
+
+
+        public dongtuplusgridViewModel ()
+        {
+            //初始化
+            Students = new ObservableCollection<StudentDataNew>() {
+                new StudentDataNew(){ Id=1, Age=11, Name="Tom" ,IsMan =true  },
+                new StudentDataNew(){ Id=2, Age=12, Name="Darren" ,IsMan =true},
+                new StudentDataNew(){ Id=3, Age=13, Name="Jacky" ,IsMan =false },
+                new StudentDataNew(){ Id=4, Age=14, Name="Andy",IsMan =false},
+                new StudentDataNew(){ Id=5, Age=15, Name="WUBingBing",IsMan =true},
+                new StudentDataNew(){ Id=6, Age=16, Name="KT",IsMan =false},
+                new StudentDataNew(){ Id=7, Age=17, Name="KT17",IsMan =false},
+                new StudentDataNew(){ Id=8, Age=18, Name="WUBing",IsMan =true},
+                new StudentDataNew(){ Id=9, Age=19, Name="Yang",IsMan =true}
+            };
+
+
+
+            AddCommand = new MyCommand( adddata );
+            ReCommand = new MyCommand( redata );
+        }
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public MyCommand AddCommand
+        {
+            get; set;
+        }
+
+        public void adddata ()
+        {
+
+            Students.Add( new StudentDataNew() { Id = 10 , Age = 20 , Name = "Yang20" , IsMan = true } );
+            Students.Add( new StudentDataNew() { Id = 11 , Age = 12 , Name = "Y12" , IsMan = false } );
+
+        }
+
+
+        public MyCommand ReCommand
+        {
+            get; set;
+        }
+
+
+        public void redata ()
+        {
+            Students.Clear();
+
+            Students.Add( new StudentDataNew() { Id = 100 , Age = 20 , Name = "Y100" , IsMan = true } );
+            Students.Add( new StudentDataNew() { Id = 110 , Age = 12 , Name = "Y110" , IsMan = false } );
+
+        }
+
+
+
+    }
+}
