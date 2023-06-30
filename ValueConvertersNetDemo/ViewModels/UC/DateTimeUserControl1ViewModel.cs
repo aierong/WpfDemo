1afd4c3831c50af970fbf54aea766f6c0c398e07
aierong <aierong@126.com>
2023-06-30 16:03:03 +0800

    0

--- ValueConvertersNetDemo/ViewModels/UC/DateTimeUserControl1ViewModel.cs
+++ ValueConvertersNetDemo/ViewModels/UC/DateTimeUserControl1ViewModel.cs
@@ -12,5 +12,26 @@ namespace ValueConvertersNetDemo.ViewModels.UC
 {
     public class DateTimeUserControl1ViewModel : BindableBase
     {
+        private DateTime _ChangeDate = DateTime.Now;
+
+        public DateTime ChangeDate
+        {
+            get
+            {
+                return _ChangeDate;
+            }
+            set
+            {
+                //SetProperty就是设置值,并且通知属性改变
+                SetProperty( ref _ChangeDate , value );
+            }
+        }
+
+        private DelegateCommand _QieHuanClickCommand;
+        public DelegateCommand QieHuanClickCommand => _QieHuanClickCommand ?? ( _QieHuanClickCommand = new DelegateCommand( () =>
+        {
+            this.ChangeDate = DateTime.Now;
+        } ) );
+
     }
 }
