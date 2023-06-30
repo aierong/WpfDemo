f5b9fdbf506ba93b0c9cfb9ce7be537d2d87ff41
aierong <aierong@126.com>
2023-06-30 09:08:32 +0800

    1

--- ValueConvertersNetDemo/ViewModels/MainWindowViewModel.cs
+++ ValueConvertersNetDemo/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Prism.Mvvm;
 
+
+/*
+nuget:ValueConverters   新版本，要求。net4.8
+
+
+*/
+
 namespace ValueConvertersNetDemo.ViewModels
 {
     public class MainWindowViewModel : BindableBase
