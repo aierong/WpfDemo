c54c5ed2c47aaa7e20d23344f04cfb5e8760fdc9
aierong <aierong@126.com>
2023-07-01 15:23:19 +0800

    1

--- ChartsDemo/ViewModels/UC/BarDongTai1ViewModel.cs
+++ ChartsDemo/ViewModels/UC/BarDongTai1ViewModel.cs
@@ -69,7 +69,7 @@ namespace ChartsDemo.ViewModels.UC
 
 
 
-            init();
+            
         }
 
 
@@ -152,137 +152,9 @@ namespace ChartsDemo.ViewModels.UC
 
 
 
-        void init ()
-        {
-            _dt_observableValues = new ObservableCollection<ObservableValue>
-            {
-                new ObservableValue(2),
+      
 
-                new ObservableValue(4),
-
-                new ObservableValue(13),
-                new ObservableValue(23),
-
-                //new(5), //  (C# 9 可以这样写)
-            };
-
-
-            dtSeries = new ObservableCollection<ISeries>()
-            {
-                new ColumnSeries<ObservableValue>
-                {
-                    Values =_dt_observableValues,
-
-                }
-            };
-
-            _dt_observableValues_x_val = new ObservableCollection<string>()
-            {
-                "王", "赵", "张", "a123"
-            };
-
-            dtXAxess = new ObservableCollection<Axis>()
-            {
-                new Axis
-            {
-
-                Labels = _dt_observableValues_x_val ,
-                //旋转角度
-                LabelsRotation = 0 ,
-                SeparatorsPaint = new SolidColorPaint( new SKColor( 200 , 200 , 200 ) ) ,
-                SeparatorsAtCenter = false ,
-                TicksPaint = new SolidColorPaint( new SKColor( 35 , 35 , 35 ) ) ,
-                TicksAtCenter = true ,
-                LabelsPaint = new SolidColorPaint
-                {
-                    Color = SKColors.Black ,
-
-                    SKTypeface = SKFontManager.Default.MatchCharacter( '汉' ) // 汉语 
-                    // SKTypeface = SKFontManager.Default.MatchCharacter('أ'), // Arab
-                    // SKTypeface = SKFontManager.Default.MatchCharacter('あ'), // Japanese
-                    // SKTypeface = SKFontManager.Default.MatchCharacter('Ж'), // Russian
-                }
-            }
-            };
-        }
-
-
-
-        private DelegateCommand _ResetButton222ClickCommand;
-        public DelegateCommand ResetButton222ClickCommand => _ResetButton222ClickCommand ?? ( _ResetButton222ClickCommand = new DelegateCommand( () =>
-        {
-            Random _random = new Random();
-            var fors = _random.Next( 2 , 8 );
-
-            _dt_observableValues.Clear();
-            _dt_observableValues_x_val.Clear();
-            for ( int i = 1 ; i <= fors ; i++ )
-            {
-                var num = _random.Next( 1 , 250 );
-
-                _dt_observableValues.Add( new ObservableValue( num ) );
-
-                _dt_observableValues_x_val.Add( num.ToString() + "系列" );
-            }
-
-
-
-            //下面是固定个数数量的
-
-            //_dt_observableValues.Clear();
-            //_dt_observableValues.Add( new ObservableValue( _random.Next( 1 , 250 ) ) );
-            //_dt_observableValues.Add( new ObservableValue( _random.Next( 44 , 55 ) ) );
-            //_dt_observableValues.Add( new ObservableValue( _random.Next( 1 , 20 ) ) );
-            //_dt_observableValues.Add( new ObservableValue( _random.Next( 1 , 10 ) ) );
-
-            //_dt_observableValues_x_val.Clear();
-            //_dt_observableValues_x_val.Add( _random.Next( 1 , 20 ).ToString() + "x" );
-            //_dt_observableValues_x_val.Add( _random.Next( 1 , 20 ).ToString() + "x" );
-            //_dt_observableValues_x_val.Add( _random.Next( 1 , 20 ).ToString() + "x" );
-            //_dt_observableValues_x_val.Add( _random.Next( 1 , 20 ).ToString() + "x" );
-
-
-            return;
-
-
-        } ) );
-
-
-        private ObservableCollection<string> _dt_observableValues_x_val;
-
-
-        private ObservableCollection<ObservableValue> _dt_observableValues = new ObservableCollection<ObservableValue>() { };
-
-        ObservableCollection<ISeries> _dtSeries;
-
-        public ObservableCollection<ISeries> dtSeries
-        {
-            get
-            {
-                return _dtSeries;
-            }
-            set
-            {
-                //SetProperty就是设置值,并且通知属性改变
-                SetProperty( ref _dtSeries , value );
-            }
-        }
-
-
-        ObservableCollection<Axis> _dtXAxess = new ObservableCollection<Axis>() { };
-
-        public ObservableCollection<Axis> dtXAxess
-        {
-            get
-            {
-                return _dtXAxess;
-            }
-            set
-            {
-                //SetProperty就是设置值,并且通知属性改变
-                SetProperty( ref _dtXAxess , value );
-            }
-        }
+       
 
 
 
