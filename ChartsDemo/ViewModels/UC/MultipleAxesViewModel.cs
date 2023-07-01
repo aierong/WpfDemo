e71dab6eb1a7de8db35169e490641a6a59732f32
aierong <aierong@126.com>
2023-07-01 14:44:45 +0800

    1

---
+++ ChartsDemo/ViewModels/UC/MultipleAxesViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace ChartsDemo.ViewModels.UC
+{
+    public class MultipleAxesViewModel : BindableBase, INavigationAware
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+INavigationAware接口3个方法:
+
+OnNavigatedFrom：导航之前触发(导航离开当前页时触发),一般用于保存该页面的数据
+OnNavigatedTo：导航后目的页面触发，一般用于初始化或者接受上页面的传递参数
+IsNavigationTarget：True则重用该View实例，Flase则每一次导航到该页面都会实例化一次
+*/
+
+        public void OnNavigatedTo ( NavigationContext navigationContext )
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool IsNavigationTarget ( NavigationContext navigationContext )
+        {
+            //throw new NotImplementedException();
+            //IsNavigationTarget：True则重用该View实例，Flase则每一次导航到该页面都会实例化一次
+            return false;
+        }
+
+        public void OnNavigatedFrom ( NavigationContext navigationContext )
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
