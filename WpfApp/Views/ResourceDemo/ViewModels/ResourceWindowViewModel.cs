8871073068331af002fb4facdf8de4f23f563fae
aierong <aierong@126.com>
2023-07-04 16:50:07 +0800

    1

--- WpfApp/Views/ResourceDemo/ViewModels/ResourceWindowViewModel.cs
+++ WpfApp/Views/ResourceDemo/ViewModels/ResourceWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using WpfApp.Properties;
 using WpfApp.Views.BaseCommand;
 
@@ -23,9 +24,12 @@ namespace WpfApp.Views.ResourceDemo.ViewModels
         {
             DataCommand = new MyCommand( () =>
             {
+
                 //viewmodel中暂时只会访问,App.xaml中定义的资源
                 var obj1 = Application.Current.TryFindResource( "ComboBoxTitle" );
 
+
+                
             } );
         }
     }
