45920e6f99df24815f4338b8b758de47ab7de226
aierong <aierong@126.com>
2023-07-06 13:36:37 +0800

    1

--- ValueConvertersNetDemo/ViewModels/UC/GroupUserControl1ViewModel.cs
+++ ValueConvertersNetDemo/ViewModels/UC/GroupUserControl1ViewModel.cs
@@ -13,7 +13,47 @@ namespace ValueConvertersNetDemo.ViewModels.UC
     public class GroupUserControl1ViewModel : BindableBase
     {
 
+        private int _Num = 0;
 
+        public int Num
+        {
+            get
+            {
+                return _Num;
+            }
+            set
+            {
+                //SetProperty就是设置值,并且通知属性改变
+                SetProperty( ref _Num , value );
+            }
+        }
+
+
+        private int _NumTwo = 100;
+
+        public int NumTwo
+        {
+            get
+            {
+                return _NumTwo;
+            }
+            set
+            {
+                //SetProperty就是设置值,并且通知属性改变
+                SetProperty( ref _NumTwo , value );
+            }
+        }
+
+        private DelegateCommand _QieHuanClickCommand;
+        public DelegateCommand QieHuanClickCommand => _QieHuanClickCommand ?? ( _QieHuanClickCommand = new DelegateCommand( () =>
+        {
+            Random _random = new Random();
+            var randomValue = _random.Next( 1 , 10 );
+
+            Num = randomValue;
+
+            NumTwo = NumTwo == 100 ? 200 : 100;
+        } ) );
 
     }
 }
