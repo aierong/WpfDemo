48a520635a57d6c49e3535454303eee2139bc99c
aierong <aierong@126.com>
2023-11-27 16:56:08 +0800

    1

---
+++ WpfApp/Views/TeXiao/LedClock/demo1/view/ClockWindow1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp.Views.TeXiao.LedClock.demo1.viewmodel;
+
+namespace WpfApp.Views.TeXiao.LedClock.demo1.view
+{
+    /// <summary>
+    /// ClockWindow1.xaml 的互動邏輯
+    /// </summary>
+    public partial class ClockWindow1 : Window
+    {
+        public ClockWindow1 ()
+        {
+            InitializeComponent();
+
+            this.DataContext = new datawindowviewmodel();
+        }
+    }
+}
---
+++ WpfApp/Views/TeXiao/LedClock/demo1/viewmodel/datawindowviewmodel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Xml.Linq;
+using WpfApp.Views.ValidationData.demo1.viewmodel;
+
+namespace WpfApp.Views.TeXiao.LedClock.demo1.viewmodel
+{
+    public class datawindowviewmodel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime timer;
+        public DateTime Timer
+        {
+            get
+            {
+                return timer;
+            }
+            set
+            {
+                timer = value;
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Timer" ) );
+            }
+        }
+
+        private void TimerEventHandler ( Object sender , EventArgs args )
+        {
+            Timer = DateTime.Now;
+        }
+
+        public datawindowviewmodel ()
+        {
+
+            Timer = DateTime.Now;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds( 1000 );
+
+            timer.Tick += new EventHandler( TimerEventHandler );
+            timer.IsEnabled = true;
+            timer.Start();
+
+        }
+    }
+}
