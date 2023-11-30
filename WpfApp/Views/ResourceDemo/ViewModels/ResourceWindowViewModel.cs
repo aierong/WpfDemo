e92cc43f7d221f9573f24ac36a80f7f885e2c5ef
aierong <aierong@126.com>
2023-11-30 11:29:11 +0800

    1

--- WpfApp/Views/ResourceDemo/ViewModels/ResourceWindowViewModel.cs
+++ WpfApp/Views/ResourceDemo/ViewModels/ResourceWindowViewModel.cs
@@ -20,6 +20,12 @@ namespace WpfApp.Views.ResourceDemo.ViewModels
             get; set;
         }
 
+        //
+        public MyCommand UCommand
+        {
+            get; set;
+        }
+
         public ResourceWindowViewModel ()
         {
             DataCommand = new MyCommand( () =>
@@ -28,8 +34,22 @@ namespace WpfApp.Views.ResourceDemo.ViewModels
                 //viewmodel中暂时只会访问,App.xaml中定义的资源
                 var obj1 = Application.Current.TryFindResource( "ComboBoxTitle" );
 
+                if ( obj1 != null )
+                {
+                }
 
-                
+            } );
+
+            UCommand = new MyCommand( () =>
+            {
+                //可以修改,App.xaml中定义的资源
+                Application.Current.Resources["RedBrush"] = new SolidColorBrush( Colors.Yellow );
+
+                Application.Current.Resources["btntext"] =  DateTime.Now.ToLongTimeString ();
+
+                //如果是ResourceWindow.xaml.cs文件请用下面方式改变资源
+                //this.Resources["RedBrush"] = new SolidColorBrush(Colors.Yellow);
+
             } );
         }
     }
