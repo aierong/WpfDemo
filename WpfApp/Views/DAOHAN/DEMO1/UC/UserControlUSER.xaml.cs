51e4f746a7fc2aa5ab71abdd4e4d1eb9a3000d38
aierong <aierong@126.com>
2023-12-04 16:24:41 +0800

    0

---
+++ WpfApp/Views/DAOHAN/DEMO1/UC/UserControlUSER.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.DAOHAN.DEMO1.UC
+{
+    /// <summary>
+    /// UserControlUSER.xaml 的互動邏輯
+    /// </summary>
+    public partial class UserControlUSER : UserControl
+    {
+        public UserControlUSER ()
+        {
+            InitializeComponent();
+        }
+    }
+}
