13b58ebc40022965ede318bd58bd2fea812f0087
aierong <aierong@126.com>
2023-12-06 10:47:57 +0800

    0

---
+++ WpfApp/Views/BindData/Binds/DataVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Views.BindData.Binds
+{
+    public class DataVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string str1;
+        public string Str1
+        {
+            get
+            {
+                return str1;
+            }
+            set
+            {
+                str1 = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Str1" ) );
+            }
+        }
+    }
+}
---
+++ WpfApp/Views/BindData/Binds/common/mydataclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Views.BindData.Binds.common
+{
+    public class mydataclass
+    {
+        public string StrName
+        {
+            get; set;
+        } = "我是公有的";
+
+        public static string title = "wo de title";
+
+        public const string constr1 = "title(常量)";
+    }
+
+}
