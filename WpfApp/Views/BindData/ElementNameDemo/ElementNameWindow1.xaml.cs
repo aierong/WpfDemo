a24962f22eea831149d113f38e76f3de2c17cc65
aierong <aierong@126.com>
2023-12-06 11:47:20 +0800

    0

---
+++ WpfApp/Views/BindData/ElementNameDemo/ElementNameWindow1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.BindData.ElementNameDemo
+{
+    /// <summary>
+    /// ElementNameWindow1.xaml 的互動邏輯
+    /// </summary>
+    public partial class ElementNameWindow1 : Window
+    {
+        public ElementNameWindow1 ()
+        {
+            InitializeComponent();
+        }
+    }
+}
