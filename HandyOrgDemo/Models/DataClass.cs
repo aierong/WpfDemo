f601efc6083d7d86a2c7ea9fa537df1a459d02ae
aierong <aierong@126.com>
2023-12-18 11:26:07 +0800

    0

---
+++ HandyOrgDemo/Models/DataClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandyOrgDemo.Models
+{
+    public class DataClass
+    {
+        public string Header
+        {
+            get; set;
+        }
+
+        public string Content
+        {
+            get; set;
+        }
+
+        public string Footer
+        {
+            get; set;
+        }
+    }
+
+}
---
+++ HandyOrgDemo/ViewModels/UserControls/DataGridUserControl1VM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using HandyOrgDemo.Models;
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public class DataGridUserControl1VM : ObservableObject
+    {
+        public ObservableCollection<DataClass> DataList
+        {
+            get; set;
+        } = new ObservableCollection<DataClass>();
+
+        public DataGridUserControl1VM ()
+        {
+            //初始化
+            DataList = new ObservableCollection<DataClass>() {
+                 new DataClass
+            {
+                Header = "Atomic",
+                Content = "1.jpg",
+                Footer = "Stive Morgan"
+            },
+            new DataClass
+            {
+                Header = "Zinderlong",
+                Content = "2.jpg",
+                Footer = "Zonderling"
+            },
+            new DataClass
+            {
+                Header = "Busy Doin' Nothin'",
+                Content = "3.jpg",
+                Footer = "Ace Wilder"
+            },
+            //new DataClass
+            //{
+            //    Header = "Wrong",
+            //    Content = "4.jpg",
+            //    Footer = "Blaxy Girls"
+            //},
+            //new DataClass
+            //{
+            //    Header = "The Lights",
+            //    Content = "5.jpg",
+            //    Footer = "Panda Eyes"
+            //},
+
+
+
+            };
+
+
+
+
+        }
+    }
+}
