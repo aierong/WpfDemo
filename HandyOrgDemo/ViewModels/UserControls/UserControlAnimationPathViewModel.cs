27b74fca1e39654d009419b06eafd25bd7ebcd76
aierong <aierong@126.com>
2023-12-29 13:21:23 +0800

    4

---
+++ HandyOrgDemo/ViewModels/UserControls/UserControlAnimationPathViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public partial class UserControlAnimationPathViewModel : ObservableObject
+    {
+
+        public UserControlAnimationPathViewModel ()
+        {
+
+        }
+
+
+        [RelayCommand()]
+        void TopButtonClick ()
+        {
+            //点击按钮 
+
+            IsOpen = !IsOpen;
+        }
+
+        [ObservableProperty]
+        private bool isOpen = false;
+
+
+    }
+}
