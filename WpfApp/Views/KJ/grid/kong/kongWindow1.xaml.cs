9b6820f8a6bc53fd45fa9afe0c898b98c8046783
aierong <aierong@126.com>
2023-12-30 17:20:36 +0800

    1

---
+++ WpfApp/Views/KJ/grid/kong/kongWindow1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.KJ.grid.kong
+{
+    /// <summary>
+    /// kongWindow1.xaml 的交互逻辑
+    /// </summary>
+    public partial class kongWindow1 : Window
+    {
+        public kongWindow1 ()
+        {
+            InitializeComponent();
+
+            this.DataContext = new kongWindow1VM();
+        }
+    }
+}
---
+++ WpfApp/Views/KJ/grid/kong/kongWindow1VM.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp.Views.BaseCommand;
+using WpfApp.Views.KJ.grid.kong.Models;
+
+namespace WpfApp.Views.KJ.grid.kong
+{
+    public class kongWindow1VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Student> Students
+        {
+            get; set;
+        } = new ObservableCollection<Student>();
+
+        public bool _HasItems = true;
+        public bool HasItems
+        {
+            get
+            {
+                return _HasItems;
+            }
+            set
+            {
+                _HasItems = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "HasItems" ) );
+            }
+        }
+
+        public kongWindow1VM ()
+        {
+            //初始化
+            Students = new ObservableCollection<Student>() {
+                new Student(){ Id=1, Age=11, Name="Tom" ,IsMan =true  },
+                new Student(){ Id=2, Age=12, Name="Darren" ,IsMan =true},
+                new Student(){ Id=3, Age=13, Name="Jacky" ,IsMan =false },
+
+            };
+
+            HasItems = true;
+
+            UpdateCommand = new MyCommand( () =>
+            {
+
+                if ( HasItems )
+                {
+                    Students.Clear();
+                }
+                else
+                {
+                    Students.Add( new Student() { Id = 1 , Age = 11 , Name = "Tom" , IsMan = true } );
+                }
+
+                HasItems = !HasItems;
+            } );
+        }
+
+        public MyCommand UpdateCommand
+        {
+            get; set;
+        }
+    }
+}
