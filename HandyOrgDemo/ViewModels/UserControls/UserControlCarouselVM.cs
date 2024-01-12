746a4701dfaf6e24011d60abd34f7915a48182a9
aierong <aierong@126.com>
2024-01-12 13:59:14 +0800

    up

---
+++ HandyOrgDemo/ViewModels/UserControls/UserControlCarouselVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using HandyOrgDemo.Models;
+
+namespace HandyOrgDemo.ViewModels.UserControls
+{
+    public partial class UserControlCarouselVM : ObservableObject
+    {
+        public ObservableCollection<string> Images
+        {
+            get; set;
+        } = new ObservableCollection<string>();
+
+        public UserControlCarouselVM ()
+        {
+            Images = new ObservableCollection<string>() {
+                "/assets/Image/lb/2.jpg" ,
+                "/assets/Image/lb/1.jpg" ,
+                "/assets/Image/lb/3.jpg"
+            };
+        }
+    }
+}
--- HandyOrgDemo/Views/UserControls/UserControlCarousel.xaml.cs
+++ HandyOrgDemo/Views/UserControls/UserControlCarousel.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HandyOrgDemo.ViewModels.UserControls;
 
 namespace HandyOrgDemo.Views.UserControls
 {
@@ -23,6 +24,8 @@ namespace HandyOrgDemo.Views.UserControls
         public UserControlCarousel ()
         {
             InitializeComponent();
+
+            this.DataContext = new UserControlCarouselVM();
         }
     }
 }
