30e9b0e7684d3c23624326a79a0444e9d236807d
aierong <aierong@126.com>
2024-02-20 11:39:07 +0800

    0

---
+++ WpfApp/Views/XamlBehaviors/XamlBehaviorsWpfWindow1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp.Views.XamlBehaviors
+{
+    /// <summary>
+    /// XamlBehaviorsWpfWindow1.xaml 的互動邏輯
+    /// </summary>
+    public partial class XamlBehaviorsWpfWindow1 : Window
+    {
+        public XamlBehaviorsWpfWindow1 ()
+        {
+            InitializeComponent();
+
+            this.DataContext = new XamlBehaviorsWpfWindow1VM();
+        }
+
+        //private void Button_MouseRightButtonDown ( object sender , MouseButtonEventArgs e )
+        //{
+
+        //}
+
+
+
+        //private void TextBlock_MouseRightButtonDown ( object sender , MouseButtonEventArgs e )
+        //{
+
+        //}
+    }
+}
---
+++ WpfApp/Views/XamlBehaviors/XamlBehaviorsWpfWindow1VM.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Views.BaseCommand;
+
+namespace WpfApp.Views.XamlBehaviors
+{
+    public class XamlBehaviorsWpfWindow1VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public MyCommand UpdateDataCommand
+        {
+            get; set;
+        }
+
+        public XamlBehaviorsWpfWindow1VM ()
+        {
+
+
+            UpdateDataCommand = new MyCommand( () =>
+            {
+
+                Str1 = DateTime.Now.ToLongTimeString();
+
+            } );
+
+            RightUpdateDataCommand = new MyCommand( () =>
+            {
+
+                Str2 = DateTime.Now.ToLongTimeString();
+
+            } );
+
+            RightDataCommand = new MyCommand( () =>
+            {
+
+                Str3 = DateTime.Now.ToLongTimeString();
+
+            } );
+        }
+
+
+        public MyCommand RightUpdateDataCommand
+        {
+            get; set;
+        }
+
+        public string str1 = "qq";
+        public string Str1
+        {
+            get
+            {
+                return str1;
+            }
+            set
+            {
+                str1 = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Str1" ) );
+            }
+        }
+
+
+        public string str2 = "鼠标右键点击我";
+        public string Str2
+        {
+            get
+            {
+                return str2;
+            }
+            set
+            {
+                str2 = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Str2" ) );
+            }
+        }
+
+
+        public string str3 = "鼠标右键点击我";
+        public string Str3
+        {
+            get
+            {
+                return str3;
+            }
+            set
+            {
+                str3 = value;
+
+                //通知数据已经变化
+                PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( "Str3" ) );
+            }
+        }
+
+        public MyCommand RightDataCommand
+        {
+            get; set;
+        }
+
+    }
+}
